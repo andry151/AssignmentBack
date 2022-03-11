9d0c2b579992830887fb09b86b75a31793453e51
micbuffa <micbuffa@gmail.com>
2022-03-11 09:50:36 +0100

    ajout de la pagination avec un plugin mongoose

--- model/assignment.js
+++ model/assignment.js
@@ -1,5 +1,6 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
+var aggregatePaginate = require("mongoose-aggregate-paginate-v2");
 
 let AssignmentSchema = Schema({
     id: Number,
@@ -7,6 +8,9 @@ let AssignmentSchema = Schema({
     nom: String,
     rendu: Boolean
 });
+
+// Pour ajouter la pagination
+AssignmentSchema.plugin(aggregatePaginate);
 
 // C'est à travers ce modèle Mongoose qu'on pourra faire le CRUD
 module.exports = mongoose.model('assignments', AssignmentSchema);
